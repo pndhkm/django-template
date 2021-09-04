d4a569032a4af98afbfb7167ad2aecd67baada60
pndhkm <panduahakm@gmail.com>
2021-09-05 06:32:36 +0700

    django + bootstrap4

---
+++ mywebsite/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.conf.urls import url
+from .views import index
+urlpatterns = [
+    url(r'admin/', admin.site.urls),
+    url(r'^$',index),
+]
---
+++ mywebsite/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title' : 'index'
+    }
+
+    return render(request,'index.html',context)
